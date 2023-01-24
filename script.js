495746b108b00235519437a8cb5d7329f1c460ce
Ivan Suhocky <vano200306@mail.ru>
2023-01-24 23:33:55 +0300

    finished piano

---
+++ script.js
@@ -0,0 +1,37 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+volumeSlider = document.querySelector(".volume-slider input"),
+keysChecked = document.querySelector(".keys-checkbox input");
+
+let allKeys = [], audio = new Audio("tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src=`tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+};
+
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+const pressedKey = (e) =>{
+   if(allKeys.includes(e.key)) playTune(e.key);
+}
+
+const handleVolume = (e) =>{
+    audio.volume = e.target.value;
+}
+
+const showHideKeys = () =>{
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+document.addEventListener("keydown", pressedKey);
+volumeSlider.addEventListener("input", handleVolume);
+keysChecked.addEventListener("click", showHideKeys);
